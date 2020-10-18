b6b5b2eb8216e0c5df5df9ac50c9d3d28131b552
Pedro Gabriel <pedrogab96@gmail.com>
2020-10-18 15:26:25 -0300

    Adicionando migrations

---
+++ database/migrations/2020_10_18_180032_created_disciplines.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatedDisciplines extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('disciplines', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->longText('difficulties')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('disciplines');
+    }
+}
