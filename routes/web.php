eac3892139a9c069dc242f03449c4efdcc2641ec
Pedro Gabriel <pedrogab96@gmail.com>
2020-10-31 16:19:47 -0300

    Adicionando a rota do resultado da busca

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DisciplineController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout.index');
 });
+
+Route::post('/search', [DisciplineController::class,'search'])->name('search');
