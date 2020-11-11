79171ed5b190384ec27bfc6a6d661aa84daa397e
dlucascabral <dlucascabral@gmail.com>
2020-11-11 03:48:02 -0300

    alterado arquivo discipline.blade.php com inserção de forms

--- resources/views/discipline.blade.php
+++ resources/views/discipline.blade.php
@@ -1,21 +1,40 @@
 @extends('layout.index')
 
 @section('content')
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-12 text-center my-4 title-subject-container">
             <h3 class="title-subject">IMD0029 - Estrutura de Dados Básicas I</h3>
         </div>
-    </div>
+    </div> --}}
 
-    <form action="#">
+    <form action="#">,
+        <div class="form-row justify-content-md-center">
+            <div class="form-group col-md-6">
+                {{-- <label for="inputSubject"><span class="text-white">Nome da disciplina</span></label> --}}
+                <h4 class="text-white">Nome da disciplina</h4>
+                <input type="text" class="form-control" id="inputSubject" placeholder="Estrutura de dados básica I">
+            </div>
+            <div class="form-group col-md-2">
+                {{-- <label for="inputCode"><span class="text-white">Código da disciplina</span></label> --}}
+                <h4 class="text-white">Código</h4>
+                <input type="text" class="form-control" id="inputCode" placeholder="IMD0029">
+            </div>
+        </div>
+
         <div class="row mt-3">
             <div class="col-12 col-sm-6">
                 <div class="form-group">
                     <h4 class="text-white">Sinopse</h4>
-                    {{-- <textarea style="resize:none" class="form-control" id="sinopse" rows="3" readonly>Iremos aprender como funciona a estrutura de dados.</textarea> --}}
-                    <div class="bg-white mt-3 p-2" style="border-radius: 8px">
+                    <textarea style="resize:none" class="form-control" id="sinopse" rows="7" placeholder="Explique aqui como funciona a disciplina."></textarea>
+                    {{-- <div class="bg-white mt-3 p-2" style="border-radius: 8px">
                         Iremos aprender como funciona a estrutura de dados.
-                    </div>
+                    </div> --}}
+
+                </div>
+
+                <div class="form-group">
+                    <h4 class="text-white">Obstáculos</h4>
+                    <textarea style="resize:none" class="form-control" id="sinopse" rows="4" placeholder="Coloque aqui problemas que alunos costumam relatar ao cursar esse componente."></textarea>
                 </div>
             </div>
 
@@ -28,21 +47,31 @@
                                 allowfullscreen></iframe>
                         </div>
                     </div>
+                    {{-- <div class="form-row mt-1 ">
+                        <input type="url" class="form-control form-control-sm" placeholder="url do vídeo">
+                    </div> --}}
+                    <div class="input-group mb-3 mt-1">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text" id="basic-addon3">youtube.com/embed/...</span>
+                        </div>
+                        <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="row mt-3">
+        {{-- <div class="row mt-3">
             <div class="col-12">
                 <div class="form-group">
-                    <h4 class="text-white">Dificuldades</h4>
+                    <h4 class="text-white">Obstáculos</h4>
                     <div class="bg-white mt-3 p-2 mb-3" style="border-radius: 8px">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis similique minus quisquam sapiente. Aspernatur repudiandae reiciendis, porro vitae officiis esse quaerat pariatur accusamus commodi, deserunt doloremque, quis laboriosam non iste. <br/> <br/>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis similique minus quisquam sapiente. Aspernatur repudiandae reiciendis, porro vitae officiis esse quaerat pariatur accusamus commodi, deserunt doloremque, quis laboriosam non iste. <br/> <br/>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis similique minus quisquam sapiente. Aspernatur repudiandae reiciendis, porro vitae officiis esse quaerat pariatur accusamus commodi, deserunt doloremque, quis laboriosam non iste.
                     </div>
+                    <textarea style="resize:none" class="form-control" id="sinopse" rows="3" placeholder="Coloque aqui problemas que alunos costumam relatar ao cursar esse componente."></textarea>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </form>
 @endsection
