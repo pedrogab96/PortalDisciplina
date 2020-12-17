b15d12bef7cfe87c4fba9f586a8a2bd68c5e0099
Victor Simplício <victor_brandao@outlook.com>
2020-12-17 17:13:37 -0300

    Instalação do serviço para youtube-embed

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Cohensive\Embed\EmbedServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -226,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Embed' => Cohensive\Embed\Facades\Embed::class,
 
     ],
 
