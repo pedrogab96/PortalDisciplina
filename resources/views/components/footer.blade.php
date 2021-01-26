462809b91b8d1bed42c2109faf062db5c263357a
Victor Simplício <victor_brandao@outlook.com>
2021-01-26 11:56:28 -0300

    Cores do footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,14 +1,14 @@
 {{-- <footer class="mt-auto bg-light text-center text-lg-start"> --}}
-<footer class="mt-auto bg-light text-center text-lg-start">
+<footer class="mt-auto bg-dark text-center text-lg-start">
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+    <div class="text-center text-light p-3" style="background-color: rgba(0, 0, 0, 0.2)">
         {{-- © 2021 Copyright: --}}
         {{-- <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> --}}
-        <a class="text-dark" href="https://www.imd.ufrn.br/portal/">IMD - Instituto Metrópole Digital</a>
+        <a class="text-light" href="https://www.imd.ufrn.br/portal/">IMD - Instituto Metrópole Digital</a>
         © 2021
 
         {{-- MODELO ALTERNATIVO --}}
-        {{-- <a class="text-dark" href="https://www.imd.ufrn.br/portal/">Instituto Metrópole Digital</a>
+        {{-- <a class="text-light" href="https://www.imd.ufrn.br/portal/">Instituto Metrópole Digital</a>
         - Natal 2021 - Todos os direitos reservados --}}
     </div>
     <!-- Copyright -->
