ff7af3521f3fd50dfb20b7ba769ab87d0e62a159
Victor Simplício <victor_brandao@outlook.com>
2021-02-06 17:54:50 -0300

    Tag HTML de verficação do Google Console Engine

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="pt_BR">
 <head>
     <meta charset="UTF-8">
+    <meta name="google-site-verification" content="CaID-sWQ4oro51-MUzVaQlu5v5a4XqK2Xpg9uVmONKI" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- bootstrap CSS --}}
