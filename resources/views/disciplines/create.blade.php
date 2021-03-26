3f709bb039ea2864ba7b6ca4a00229dc46fc662e
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-03-26 18:16:26 -0300

    refactor: disciplines.create

--- resources/views/disciplines/create.blade.php
+++ resources/views/disciplines/create.blade.php
@@ -1,214 +1,218 @@
 @extends('layouts.app')
 
 @section('title')
-Cadastrar disciplina - Portal das Disciplinas IMD
+    Cadastrar disciplina - Portal das Disciplinas IMD
 @endsection
 
 @section('robots')
-noindex, follow
+    noindex, follow
 @endsection
 
 @section('content')
+    <h4 class="text-white">Registrar nova disciplina</h4>
     <form action="{{ route("disciplinas.store") }}" method="post">
         @csrf
-        <div class="form-row justify-content-md-center">
+        <div class="form-row">
             <div class="form-group col-md-10">
-                <h4 class="text-white">Nome da disciplina</h4>
+                <label class="text-white" for="name">
+                    Nome da disciplina
+                </label>
                 <input type="text"
-                    required
-                    class="form-control {{ $errors->has('inputSubject') ? 'is-invalid' : ''}}"
-                    id="inputSubject"
-                    name="inputSubject"
-                    placeholder="ex: Estrutura de dados básica I">
-
-                @if ($errors->has('inputSubject'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('inputSubject') }}
-                    </div>
-                @endif
+                       required
+                       class="form-control {{ $errors->has('name') ? 'is-invalid' : ''}}"
+                       id="name"
+                       name="name"
+                       value="{{old('name')}}"
+                       placeholder="Estrutura de dados básica I">
+                @error('name')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="form-group col-md-2">
-                <h4 class="text-white">Código</h4>
+                <label class="text-white" for="code">
+                    Código
+                </label>
                 <input type="text"
-                    required
-                    class="form-control {{ $errors->has('inputCode') ? 'is-invalid' : ''}}"
-                    id="inputCode"
-                    name="inputCode"
-                    placeholder="IMD0000">
-
-                @if ($errors->has('inputCode'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('inputCode') }}
-                    </div>
-                @endif
-            </div>
-
-            <div class="form-group col-md-8">
-                <h4 class="text-white">Professor</h4>
-                <input type="text"
-                    required
-                    class="form-control {{ $errors->has('teacher') ? 'is-invalid' : ''}}"
-                    id="teacher"
-                    name="teacher"
-                    placeholder="Professor que irá lecionar a disciplina">
-
-                @if ($errors->has('teacher'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('teacher') }}
-                    </div>
-                @endif
-            </div>
-
-            <div class="form-group col-md-4">
-                <h4 class="text-white">Email</h4>
-                <input type="text"
-                    required
-                    class="form-control {{ $errors->has('teacherEmail') ? 'is-invalid' : ''}}"
-                    id="teacherEmail"
-                    name="teacherEmail"
-                    placeholder="Email do professor responsável pela disciplina">
-
-                @if ($errors->has('teacherEmail'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('teacherEmail') }}
-                    </div>
-                @endif
+                       required
+                       class="form-control {{ $errors->has('code') ? 'is-invalid' : ''}}"
+                       id="code"
+                       name="code"
+                       value="{{old('code')}}"
+                       placeholder="IMD0000">
+                @error('code')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
         </div>
 
-        <div class="row mt-3">
-            <div class="col-12 col-sm-6">
+        <div class="form-row">
+            <div class="form-group col-md-8">
+                <label class="text-white" for="professor-name">
+                    Nome do professor que irá lecionar a disciplina
+                </label>
+                <input type="text"
+                       required
+                       class="form-control {{ $errors->has('professor-name') ? 'is-invalid' : ''}}"
+                       id="professor-name"
+                       name="professor-name"
+                       value="{{old('professor-name')}}"
+                       placeholder="Nome do professor que irá lecionar a disciplina">
+                @error('professor-name')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group col-md-4">
+                <label class="text-white" for="professor-email">
+                    E-mail do professor
+                </label>
+                <input type="text"
+                       required
+                       class="form-control {{ $errors->has('professor-email') ? 'is-invalid' : ''}}"
+                       id="professor-email"
+                       name="professor-email"
+                       value="{{old('professor-email')}}"
+                       placeholder="professor@gmail.com">
+                @error('professor-email')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+        </div>
+
+        <div class="form-row mt-3">
+            <div class="col-md-6">
                 <div class="form-group">
-                    <h4 class="text-white">Sinopse</h4>
-                    <textarea style="resize:none"
-                        class="form-control {{ $errors->has('sinopse') ? 'is-invalid' : ''}}"
-                        id="sinopse"
-                        name="sinopse"
-                        rows="7"
-                        placeholder="Explique aqui como funciona a disciplina."></textarea>
+                    <label class="text-white" for="synopsis">
+                        Sinopse
+                    </label>
+                    <textarea
+                        class="form-control {{ $errors->has('synopsis') ? 'is-invalid' : ''}}"
+                        id="synopsis"
+                        name="synopsis"
+                        rows="8"
+                        placeholder="Explique aqui como funciona a disciplina">{{old('synopsis')}}</textarea>
+                    @error('synopsis')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
 
-                    @if ($errors->has('sinopse'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('sinopse') }}
-                        </div>
-                    @endif
+                <div class="form-group mt-3">
+                    <label class="text-white" for="classificacao">
+                        Classificação
+                    </label>
+                    <input type="text"
+                           class="form-control {{ $errors->has('classificacao') ? 'is-invalid' : ''}}"
+                           name="classificacao"
+                           id="classificacao"
+                           value="{{old('classificacao')}}"
+                           aria-describedby="basic-addon3"
+                           placeholder="Link para imagem no Google Drive">
+                    @error('classificacao')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
 
-                    <h4 class="text-white mt-3">Classificação</h4>
-                    <div class="input-group mb-3 mt-1">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label class="text-white" for="media-trailer">
+                        Trailer da disciplina
+                    </label>
+                    <div class="input-group">
                         <input type="text"
-                            class="form-control {{ $errors->has('classificacao') ? 'is-invalid' : ''}}"
-                            name="classificacao"
-                            id="classificacao"
-                            aria-describedby="basic-addon3"
-                            placeholder="Link para imagem no Google Drive">
-
-                        @if ($errors->has('classificacao'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('classificacao') }}
-                            </div>
-                        @endif
+                               class="form-control {{ $errors->has('media-trailer') ? 'is-invalid' : ''}}"
+                               name="media-trailer"
+                               id="media-trailer"
+                               value="{{old('media-trailer')}}"
+                               aria-describedby="basic-addon3"
+                               placeholder="Link para vídeo no Youtube">
+                        @error('media-trailer')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
 
-            </div>
-
-            <div class="col-12 col-sm-6">
                 <div class="form-group">
-                    <h4 class="text-white">Trailer da disciplina</h4>
-                    <div class="input-group mb-3 mt-1">
+                    <label class="text-white" for="media-video">
+                        Vídeo
+                    </label>
+                    <div class="input-group">
                         <input type="text"
-                            class="form-control {{ $errors->has('trailer') ? 'is-invalid' : ''}}"
-                            name="trailer"
-                            id="basic-url"
-                            aria-describedby="basic-addon3"
-                            placeholder="Link para vídeo no Youtube">
-
-                        @if ($errors->has('trailer'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('trailer') }}
-                            </div>
-                        @endif
+                               class="form-control {{ $errors->has('media-video') ? 'is-invalid' : ''}}"
+                               name="media-video"
+                               id="media-video"
+                               value="{{old('media-video')}}"
+                               aria-describedby="basic-addon3"
+                               placeholder="Link para vídeo no Youtube">
+                        @error('media-video')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
-
-                    <h4 class="text-white">Vídeo</h4>
-                    <div class="input-group mb-3 mt-1">
+                </div>
+                <div class="form-group">
+                    <label class="text-white" for="media-podcast">
+                        Podcast
+                    </label>
+                    <div class="input-group">
                         <input type="text"
-                            class="form-control {{ $errors->has('video') ? 'is-invalid' : ''}}"
-                            name="video"
-                            id="basic-url"
-                            aria-describedby="basic-addon3"
-                            placeholder="Link para vídeo no Youtube">
-
-                        @if ($errors->has('video'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('video') }}
-                            </div>
-                        @endif
+                               class="form-control {{ $errors->has('media-podcast') ? 'is-invalid' : ''}}"
+                               name="media-podcast"
+                               id="media-podcast"
+                               value="{{old('media-podcast')}}"
+                               aria-describedby="basic-addon3"
+                               placeholder="Link para podcast no Youtube">
+                        @error('media-podcast')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
-
-                    <h4 class="text-white">Podcast</h4>
-                    <div class="input-group mb-3 mt-1">
+                </div>
+                <div class="form-group">
+                    <label class="text-white" for="media-material">
+                        Materiais
+                    </label>
+                    <div class="input-group">
                         <input type="text"
-                            class="form-control {{ $errors->has('podcast') ? 'is-invalid' : ''}}"
-                            name="podcast"
-                            id="basic-url"
-                            aria-describedby="basic-addon3"
-                            placeholder="Link para podcast no Youtube">
-
-                        @if ($errors->has('podcast'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('podcast') }}
-                            </div>
-                        @endif
-                    </div>
-
-                    <h4 class="text-white">Materiais</h4>
-                    <div class="input-group mb-3 mt-1">
-                        <input type="text"
-                            class="form-control {{ $errors->has('materiais') ? 'is-invalid' : ''}}"
-                            name="materiais"
-                            id="basic-url"
-                            aria-describedby="basic-addon3"
-                            placeholder="Link para arquivo no Google Drive">
-
-                        @if ($errors->has('materiais'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('materiais') }}
-                            </div>
-                        @endif
+                               class="form-control {{ $errors->has('media-material') ? 'is-invalid' : ''}}"
+                               name="media-material"
+                               id="media-material"
+                               value="{{old('media-material')}}"
+                               aria-describedby="basic-addon3"
+                               placeholder="Link para arquivo no Google Drive">
+                        @error('media-material')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="form-row mt-3">
-            <div class="col-12">
-                <h4 class="text-white">Obstáculos</h4>
-                <div class="form-row">
-
-                    <textarea style="resize:none"
-                        class="form-control {{ $errors->has('sinopse') ? 'is-invalid' : ''}}"
-                        id="obstaculos"
-                        name="obstaculos"
-                        rows="4"
-                        placeholder="Coloque aqui problemas que alunos costumam relatar ao cursar esse componente."></textarea>
-
-                    @if ($errors->has('obstaculos'))
-                        <div class="invalid-feedback">
-                            {{ $errors->first('obstaculos') }}
-                        </div>
-                    @endif
-
+            <div class="col-md-12">
+                <div class="form-group">
+                    <label class="text-white" for="difficulties">
+                        Obstáculos
+                    </label>
+                    <div class="input-group">
+                        <textarea style="resize:none"
+                                  class="form-control {{ $errors->has('difficulties') ? 'is-invalid' : ''}}"
+                                  id="difficulties"
+                                  name="difficulties"
+                                  rows="4"
+                                  placeholder="Coloque aqui problemas que alunos costumam relatar ao cursar esse componente.">{{old('difficulties')}}</textarea>
+                        @error('difficulties')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="d-flex p-2 mb-5 justify-content-center">
-            <button type="submit" class="btn btn-primary btn-sm mx-2">Salvar</button>
-            <a href="{{ route("home") }}" class="btn btn-danger btn-sm mx-2" >
+        <div class="row d-flex p-2 mt-3 justify-content-center">
+            <a href="{{ route('home') }}" class="btn btn-danger btn-sm">
                 Cancelar
             </a>
+            <button type="submit" class="btn btn-primary btn-sm ml-5">Registrar</button>
         </div>
     </form>
 
