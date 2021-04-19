1d6d93942647f878d2417f8341cec3ab5c189a76
Victor <victor_brandao@outlook.com>
2021-04-19 15:40:57 -0300

    feat: video na homepage

--- resources/views/disciplines/index.blade.php
+++ resources/views/disciplines/index.blade.php
@@ -7,6 +7,14 @@
         </div>
     </div>
 
+    <div class="row">
+        <div class="col-12">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/RVhnKTOTWYs" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+
     {{-- Modificar para apenas o user adm ou professor --}}
 
     @auth
