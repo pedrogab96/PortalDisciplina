4d0c70bfa11d55e024c5fe6fc87eabf945ccbd4c
Victor <victor_brandao@outlook.com>
2021-05-20 13:05:50 -0300

    refactor: experimentações no footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,15 +1,29 @@
-{{-- <footer class="mt-auto bg-light text-center text-lg-start"> --}}
-<footer class="mt-auto bg-dark text-center text-lg-start">
-    <!-- Copyright -->
-    <div class="text-center text-light p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        {{-- © 2021 Copyright: --}}
-        {{-- <a class="text-dark" href="https://mdbootstrap.com/">MDBootstrap.com</a> --}}
-        <a class="text-light" href="https://www.imd.ufrn.br/portal/">IMD - Instituto Metrópole Digital</a>
-        © 2021
+{{-- <footer id="footer" class="mt-auto bg-dark text-center text-lg-start"> --}}
+<footer id="footer" class="mt-auto text-center text-lg-start">
+    <div class="container d-md-flex py-4">
+        <div class="mr-md-auto text-center text-md-left">
+          <div class="copyright">
+            &copy; Copyright <strong><span>Mentor</span></strong>. All Rights Reserved
+          </div>
+          <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+          </div>
 
-        {{-- MODELO ALTERNATIVO --}}
-        {{-- <a class="text-light" href="https://www.imd.ufrn.br/portal/">Instituto Metrópole Digital</a>
-        - Natal 2021 - Todos os direitos reservados --}}
+          <!-- TODO footer IMD 1 -->
+          <div class="credits">
+              <a href="https://www.imd.ufrn.br/portal/">IMD - Instituto Metrópole Digital</a>
+              © 2021
+          </div>
+        </div>
+
+         <!-- TODO footer IMD 2 -->
+         {{-- <div class="text-center py-3">
+            <a href="https://www.imd.ufrn.br/portal/">IMD - Instituto Metrópole Digital</a>
+            © 2021
+        </div> --}}
     </div>
-    <!-- Copyright -->
 </footer>
