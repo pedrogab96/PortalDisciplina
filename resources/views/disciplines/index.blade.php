e879b211306375130df8049ca82fbee9ec3677f5
Victor <victor_brandao@outlook.com>
2021-05-20 13:28:22 -0300

    refactor: tamanho do vídeo introdutório

--- resources/views/disciplines/index.blade.php
+++ resources/views/disciplines/index.blade.php
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-lg-4">
+            <div class="col-lg-6">
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/qG4ATq0qJlE"
                             allowfullscreen></iframe>
