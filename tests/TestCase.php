c1c20fb21c2ba00563be47ce360f54afb0b0642a
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2021-05-26 18:17:47 -0300

    test: configuration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Tests;
 
+use Faker\Factory;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @var \Faker\Generator
+     */
+    protected $faker;
+
+    /**
+     * @var string
+     */
+    protected $path = '/api/v1/';
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('db:seed');
+        $this->faker = Factory::create('pt_BR');
+    }
 }
