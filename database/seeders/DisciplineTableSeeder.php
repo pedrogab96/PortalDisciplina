2eb3e655892ef51b48819d9d0ae861ed1af68a47
Arthur Sérvulo <31857271+arthurmalakian@users.noreply.github.com>
2021-07-31 16:27:36 -0300

    fix: Discipline seeders

--- database/seeders/DisciplineTableSeeder.php
+++ database/seeders/DisciplineTableSeeder.php
@@ -16,37 +16,9 @@ class DisciplineTableSeeder extends Seeder
      */
     public function run()
     {
-        Discipline::create([
-            'code' => 'IMD0029',
-            'name' => 'Estrutura de dados básicas I',
-            'description'=>'Nessa disciplina estudaremos como estruturar dados.',
-            'difficulties' => 'As dificuldades que os alunos encontram são programar com c++',
-            'professor_id' => 1
-        ]);
-
-        Discipline::create([
-            'code' => 'IMD0030',
-            'name' => 'Estrutura de dados básicas II',
-            'description'=>'Nessa disciplina iremos continuar o estudo sobre estrutura de dados.',
-            'difficulties' => 'As dificuldades que os alunos encontram são programar com c++',
-            'professor_id' => 1
-        ]);
-
-        Discipline::create([
-            'code' => 'IMD0029',
-            'name' => 'Estrutura de dados básicas I',
-            'description'=>'Iremos aprender como funciona a estrutura de dados.',
-            'difficulties' => 'As dificuldades que os alunos encontram são programar com c++',
-            'professor_id' => 1
-        ]);
-
-        Discipline::create([
-            'code' => 'IMD0031',
-            'name' => 'Vetores e Geometria Analítica',
-            'description'=>'Iremos aprender funções vetoriais e geometria',
-            'difficulties' => 'As dificuldades que os alunos encontram são ter um conhecimento básico em vetores e geometria.',
-            'professor_id' => 1
-        ]);
+       for($i = 0; $i < 4; $i++){
+            Discipline::factory()->create();
+       }
 
     }
 }
